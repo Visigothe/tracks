f758f4406ebb09fab4efee501916f4d0dbb9609f
Brian Multhauf <brian.multhauf@gmail.com>
2013-10-11 13:35:48 -0400

    Pair down to one method to create two instance objects.

--- app/models/stats/tag_cloud.rb
+++ app/models/stats/tag_cloud.rb
@@ -3,10 +3,9 @@
 
 class TagCloud
 
-  attr_reader :user, :tags, :min, :divisor,
-              :tags_90days, :min_90days, :divisor_90days
+  attr_reader :user, :tags, :min, :divisor
 
-  def initialize(user, cut_off)
+  def initialize(user, cut_off=nil)
     @user = user
     @cut_off = cut_off
   end
@@ -15,7 +14,10 @@ class TagCloud
     # TODO: parameterize limit
     levels=10
 
-    params = [sql, user.id]
+    params = [sql(@cut_off), user.id]
+    if @cut_off
+      params += [@cut_off, @cut_off]
+    end
     @tags = Tag.find_by_sql(params).sort_by { |tag| tag.name.downcase }
 
     max, @min = 0, 0
@@ -25,17 +27,6 @@ class TagCloud
     }
 
     @divisor = ((max - @min) / levels) + 1
-
-    params = [sql(@cut_off), user.id, @cut_off, @cut_off]
-    @tags_90days = Tag.find_by_sql(params).sort_by { |tag| tag.name.downcase }
-
-    max_90days, @min_90days = 0, 0
-    @tags_90days.each { |t|
-      max_90days = [t.count.to_i, max_90days].max
-      @min_90days = [t.count.to_i, @min_90days].min
-    }
-
-    @divisor_90days = ((max_90days - @min_90days) / levels) + 1
   end
 
   private
@@ -44,7 +35,6 @@ class TagCloud
     query = "SELECT tags.id, tags.name AS name, count(*) AS count"
     query << " FROM taggings, tags, todos"
     query << " WHERE tags.id = tag_id"
-
     query << " AND todos.user_id=? "
     query << " AND taggings.taggable_type='Todo' "
     query << " AND taggings.taggable_id=todos.id "
